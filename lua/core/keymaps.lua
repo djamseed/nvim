b33346d4df40c4d871a1e0a659c2013e49bc24cd
Djamseed Khodabocus <djamseed86@gmail.com>
2024-09-09 02:23:01 +0400

    chore: update keymaps to use Ctrl + Alt to move lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -37,13 +37,13 @@ map({ "i", "n", "s", "x" }, "<C-s>", "<CMD>w<CR><ESC>", { desc = "Save buffer wi
 
 -- Editing
 --
--- Use Alt + j/k to move lines up/down
-map("i", "<A-k>", "<ESC><CMD>m .-2<CR>==gi", { desc = "Move line(s) up" })
-map("i", "<A-j>", "<ESC><CMD>m .+1<CR>==gi", { desc = "Move lines(s) down" })
-map("n", "<A-k>", "<CMD>m .-2<CR>==", { desc = "Move line(s) up" })
-map("n", "<A-j>", "<CMD>m .+1<CR>==", { desc = "Move line(s) down" })
-map("v", "<A-k>", "<CMD>m '<-2<CR>gv=gv", { desc = "Move line(s) up" })
-map("v", "<A-j>", "<CMD>m '>+1<CR>gv=gv", { desc = "Move line(s) down" })
+-- Use Ctrl + Alt + j/k to move lines up/down
+map("i", "<C-A-k>", "<ESC><CMD>m .-2<CR>==gi", { desc = "Move line(s) up" })
+map("i", "<C-A-j>", "<ESC><CMD>m .+1<CR>==gi", { desc = "Move lines(s) down" })
+map("n", "<C-A-k>", "<CMD>m .-2<CR>==", { desc = "Move line(s) up" })
+map("n", "<C-A-j>", "<CMD>m .+1<CR>==", { desc = "Move line(s) down" })
+map("v", "<C-A-k>", "<CMD>m '<-2<CR>gv=gv", { desc = "Move line(s) up" })
+map("v", "<C-A-j>", "<CMD>m '>+1<CR>gv=gv", { desc = "Move line(s) down" })
 
 -- Identing
 map("n", ">", ">>", { desc = "Shift text to the right" })
