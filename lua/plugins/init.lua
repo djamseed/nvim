414f762eddf99ab592a6597ecb0119a3032ba170
Djamseed Khodabocus <djamseed86@gmail.com>
2026-03-15 03:44:10 +0400

    chore: remove redudant comment plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,16 +17,4 @@ return {
     },
     -- Neovim notifications and LSP progress messages
     { 'j-hui/fidget.nvim', event = 'LspAttach', opts = { notification = { window = { winblend = 0 } } } },
-    -- Smart and Powerful commenting
-    -- NORMAL mode
-    --     gcc -> Linewise comment
-    --     gbc -> Blockwise comment
-    --     [count]gcc -> Number of lines as prefix + linewise comment
-    --     [count]gbc -> Number of lines as prefix + blockwise comment
-    --     gc[count]{motion} -> Coment the region with linewise comment
-    --     hb[count]{motion} -> Comment the region with blockwise comment
-    --  VISUAL Mode
-    --      gc -> Comment the region with linewise comment
-    --      gb -> Comment the region with blockwise comment
-    { 'numToStr/Comment.nvim', event = { 'BufReadPost', 'BufNewFile' }, opts = {} },
 }
