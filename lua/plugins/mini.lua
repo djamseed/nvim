da00daed8101cf770f23147e1ccf8bdf679c1edc
Djamseed Khodabocus <djamseed86@gmail.com>
2026-05-09 01:03:09 +0400

    refactor: change key prefix from s to gz for mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -25,10 +25,20 @@ return {
     -- add character pairs automatically
     { 'echasnovski/mini.pairs', version = '*', event = { 'InsertEnter' }, opts = {} },
     -- add, delete, replace, find, highlight surrounding (brakets, quotes, etc)
-    --     add surrounding with sa (in visual mode or on motion).
-    --     delete surrounding with sd.
-    --     replace surrounding with sr.
-    --     find surrounding with sf or sF (move cursor right or left).
-    --     highlight surrounding with sh.
-    { 'echasnovski/mini.surround', version = '*', event = { 'BufReadPost', 'BufNewFile' }, opts = {} },
+    {
+        'echasnovski/mini.surround',
+        version = '*',
+        event = { 'BufReadPost', 'BufNewFile' },
+        opts = {
+            mappings = {
+                add = 'gza', -- add surrounding
+                delete = 'gzd', -- delete surrounding
+                find = 'gzf', -- find surrounding (right)
+                find_left = 'gzF', -- find surrounding (left)
+                highlight = 'gzh', -- highlight surrounding
+                replace = 'gzr', -- replace surrounding
+                update_n_lines = 'gzn', -- update n_lines
+            },
+        },
+    },
 }
