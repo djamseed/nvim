60f10db1fabd839408a045c8ec2cacbece1a2037
Djamseed Khodabocus <djamseed86@gmail.com>
2026-08-21 17:55:37 +0400

    fix: make shell formatting honour a 2-space indent

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -44,7 +44,6 @@ conform.setup({
     },
     formatters = {
         csharpier = function() return csharpier_config() end,
-        shfmt = { prepend_args = { '-i', '2' } },
     },
     notify_on_error = false,
 })
